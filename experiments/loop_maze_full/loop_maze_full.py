f780dc51b4519f563ba451a46775fe5f3eeb463a
khanhvu207 <ngkhanhvu207@gmail.com>
2022-01-24 02:16:49 +0100

    init

---
+++ experiments/loop_maze_full/loop_maze_full.py
@@ -0,0 +1,430 @@
+import copy
+from abc import ABC
+
+import matplotlib
+import matplotlib.pyplot as plt
+from matplotlib.offsetbox import AnchoredOffsetbox, TextArea, DrawingArea, HPacker
+from matplotlib.patches import Rectangle
+
+from core.environment import Environment
+from core.utils.helper_functions import polar2euclid
+from core.utils.seed_everything import *
+
+
+class MyEnv(Environment, ABC):
+    VALIDATION_MODE = 0
+
+    def __init__(self, size_x, size_y, device, debug=False, **kwargs):
+        self.device = device
+        self._mode = -1
+        self._mode_score = 0.0
+        self._mode_episode_count = 0
+        self._size_maze_x = size_x
+        self._size_maze_y = size_y
+        self._higher_dim_obs = kwargs["higher_dim_obs"]
+        self.intern_dim = 2
+        self.debug = debug
+
+        self.default_agent_pos = [size_x // 2, size_y // 2]
+        self.create_map()
+
+    def _get_agent_pos(self):
+        random_pos_x = random.randint(1, self._size_maze_x - 2)
+        random_pos_y = random.randint(0, self._size_maze_y - 1)
+        return [random_pos_x, random_pos_y]
+        # return self.default_agent_pos
+
+    def create_map(self):
+        self._map = np.zeros((self._size_maze_x, self._size_maze_y))
+        self._pos_agent = self._get_agent_pos()
+        self._pos_goal = [self._size_maze_x - 2, self._size_maze_y - 2]
+        # self._map[-1, :] = 1
+        # self._map[0, :] = 1
+        # self._map[:, 0] = 1
+        # self._map[:, -1] = 1
+
+    def reset(self, mode):
+        self.create_map()
+
+        if mode == MyEnv.VALIDATION_MODE:
+            if self._mode != MyEnv.VALIDATION_MODE:
+                self._mode = MyEnv.VALIDATION_MODE
+                self._mode_score = 0.0
+                self._mode_episode_count = 0
+            else:
+                self._mode_episode_count += 1
+        elif self._mode != -1:
+            self._mode = -1
+
+        self._pos_agent = self._get_agent_pos()
+        return [1 * [self._size_maze_x * [self._size_maze_y * [0]]]]
+
+    def act(self, action) -> float:
+        """Applies the agent action [action] on the environment.
+
+        Parameters
+        -----------
+        action : int
+            The action selected by the agent to operate on the environment. Should be an identifier
+            included between 0 included and nActions() excluded.
+        """
+        self._cur_action = action
+        new_x, new_y = self._pos_agent[0], self._pos_agent[1]
+
+        def _modulo(x, base):
+            return (x + base) % base
+
+        # NOTE: x is vertical axis
+        if action == 0:
+            new_x = _modulo(new_x - 1, self._size_maze_x)
+            # new_x -= 1
+        elif action == 1:
+            new_x = _modulo(new_x + 1, self._size_maze_x)
+            # new_x += 1
+        elif action == 2:
+            new_y = _modulo(new_y - 1, self._size_maze_y)
+            # new_y -= 1
+        elif action == 3:
+            new_y = _modulo(new_y + 1, self._size_maze_y)
+            # new_y += 1
+
+        # If the next position is unoccupied
+        if self._map[new_x, new_y] == 0:
+            self._pos_agent[0] = new_x
+            self._pos_agent[1] = new_y
+
+        # There is no reward in this simple environment
+        reward = 0
+        self._mode_score += reward
+
+        return reward
+
+    def get_input_dims(self):
+        if self._higher_dim_obs:
+            return [(1, self._size_maze_x * 6, self._size_maze_y * 6)]
+        else:
+            return [(1, self._size_maze_x, self._size_maze_y)]
+
+    def observation_type(self, subject):
+        return np.float
+
+    def get_num_action(self):
+        return 4
+
+    def observe(self):
+        obs = copy.deepcopy(self._map)
+        obs[self._pos_agent[0], self._pos_agent[1]] = 0.5
+
+        if self._higher_dim_obs:
+            obs = self.get_higher_dim_obs([self._pos_agent], [self._pos_goal])
+
+        if self.debug is True:
+            plt.imshow(obs, cmap="gray_r")
+            plt.show()
+
+        return [obs]
+
+    def get_higher_dim_obs(self, indices_agent, indices_reward):
+        """Obtain the high-dimensional observation from indices of the agent position and the indices of the reward
+        positions."""
+        obs = copy.deepcopy(self._map)
+        obs = obs / 1.0
+        obs = np.repeat(np.repeat(obs, 6, axis=0), 6, axis=1)
+
+        # Agent representation
+        agent_obs = np.zeros((6, 6))
+        agent_obs[0, 2] = 0.7
+        agent_obs[1, 0:5] = 0.8
+        agent_obs[2, 1:4] = 0.8
+        agent_obs[3, 1:4] = 0.8
+        agent_obs[4, 1] = 0.8
+        agent_obs[4, 3] = 0.8
+        agent_obs[5, 0:2] = 0.8
+        agent_obs[5, 3:5] = 0.8
+
+        # Reward representation
+        reward_obs = np.zeros((6, 6))
+
+        for i in indices_reward:
+            obs[i[0] * 6: (i[0] + 1) * 6:, i[1] * 6: (i[1] + 1) * 6] = reward_obs
+
+        for i in indices_agent:
+            obs[i[0] * 6: (i[0] + 1) * 6:, i[1] * 6: (i[1] + 1) * 6] = agent_obs
+
+        return obs
+
+    def in_terminal_state(self) -> bool:
+        # Uncomment the following lines to add some cases where the episode terminates.
+        # This is used to show how the environment representation interpret cases where
+        # part of the environment could not be explored.
+        #        if((self._pos_agent[0]<=1 and self._cur_action==0) ):
+        #            return True
+        return False
+
+    def summarize_performance(self, test_data_set, learning_algo, *args, **kwargs):
+        """Plot of the low-dimensional representation of the environment built by the model"""
+        all_possible_inputs = []
+        self.create_map()
+
+        for y_a in range(self._size_maze_y):
+            for x_a in range(self._size_maze_x):
+                state = copy.deepcopy(self._map)
+
+                if state[x_a, y_a] == 0:
+                    if self._higher_dim_obs:
+                        all_possible_inputs.append(
+                            self.get_higher_dim_obs([[x_a, y_a]], [self._pos_goal])
+                        )
+                    else:
+                        state[x_a, y_a] = 0.5
+                        all_possible_inputs.append(state)
+
+        all_possible_inputs = np.expand_dims(np.array(all_possible_inputs, dtype="float"), axis=1)
+        all_possible_inputs = torch.from_numpy(all_possible_inputs).float().to(self.device)
+        all_possible_abs_states = learning_algo.encoder(all_possible_inputs)
+        all_possible_abs_states = all_possible_abs_states.detach().cpu().numpy()
+
+        # if not self.in_terminal_state():
+        #     self._mode_episode_count += 1
+        # print(
+        #     "== Mean score per episode is {} over {} episodes ==".format(
+        #         self._mode_score / (self._mode_episode_count + 0.0001),
+        #         self._mode_episode_count,
+        #     )
+        # )
+
+        # cm.ScalarMappable(cmap=cm.jet)
+        # abs_states = abs_states.detach().cpu().numpy()
+
+        x = np.array(all_possible_abs_states)[:, 0]
+        y = np.array(all_possible_abs_states)[:, 1]
+        if self.intern_dim > 2:
+            z = np.array(all_possible_abs_states)[:, 2]
+
+        fig = plt.figure()
+        if self.intern_dim == 2:
+            ax = fig.add_subplot(111)
+            ax.set_xlabel(r"$X_1$")
+            ax.set_ylabel(r"$X_2$")
+        else:
+            ax = fig.add_subplot(111, projection="3d")
+            ax.set_xlabel(r"$X_1$")
+            ax.set_ylabel(r"$X_2$")
+            ax.set_zlabel(r"$X_3$")
+
+        # Plot the estimated transitions
+        for i in range(len(all_possible_abs_states)):
+            predicted_action0 = (
+                learning_algo.transition(
+                    torch.cat(
+                        (
+                            torch.from_numpy(all_possible_abs_states[i: i + 1]).float(),
+                            torch.from_numpy(np.array([[1, 0, 0, 0]])).float(),
+                        ),
+                        -1,
+                    ).to(self.device)
+                )
+                .detach()
+                .cpu()
+                .numpy()
+            )
+            predicted_action1 = (
+                learning_algo.transition(
+                    torch.cat(
+                        (
+                            torch.from_numpy(all_possible_abs_states[i: i + 1]).float(),
+                            torch.from_numpy(np.array([[0, 1, 0, 0]])).float(),
+                        ),
+                        -1,
+                    ).to(self.device)
+                )
+                .detach()
+                .cpu()
+                .numpy()
+            )
+            predicted_action2 = (
+                learning_algo.transition(
+                    torch.cat(
+                        (
+                            torch.from_numpy(all_possible_abs_states[i: i + 1]).float(),
+                            torch.from_numpy(np.array([[0, 0, 1, 0]])).float(),
+                        ),
+                        -1,
+                    ).to(self.device)
+                )
+                .detach()
+                .cpu()
+                .numpy()
+            )
+            predicted_action3 = (
+                learning_algo.transition(
+                    torch.cat(
+                        (
+                            torch.from_numpy(all_possible_abs_states[i: i + 1]).float(),
+                            torch.from_numpy(np.array([[0, 0, 0, 1]])).float(),
+                        ),
+                        -1,
+                    ).to(self.device)
+                )
+                .detach()
+                .cpu()
+                .numpy()
+            )
+
+            if self.intern_dim == 2:
+                r1, t1 = x[i: i + 1], y[i: i + 1]
+                x1, y1 = polar2euclid(r1, t1)
+                r2, t2 = predicted_action0[0, :1], predicted_action0[0, 1:2]
+                x2, y2 = polar2euclid(r2, t2)
+                ax.plot(
+                    np.concatenate([x1, x2]),
+                    np.concatenate([y1, y2]),
+                    # np.concatenate([x[i: i + 1], predicted1[0, :1]]),
+                    # np.concatenate([y[i: i + 1], predicted1[0, 1:2]]),
+                    color="0.9",
+                    alpha=0.75,
+                )
+                r2, t2 = predicted_action1[0, :1], predicted_action1[0, 1:2]
+                x2, y2 = polar2euclid(r2, t2)
+                ax.plot(
+                    np.concatenate([x1, x2]),
+                    np.concatenate([y1, y2]),
+                    # np.concatenate([x[i: i + 1], predicted2[0, :1]]),
+                    # np.concatenate([y[i: i + 1], predicted2[0, 1:2]]),
+                    color="0.65",
+                    alpha=0.75,
+                )
+                r2, t2 = predicted_action2[0, :1], predicted_action2[0, 1:2]
+                x2, y2 = polar2euclid(r2, t2)
+                ax.plot(
+                    np.concatenate([x1, x2]),
+                    np.concatenate([y1, y2]),
+                    # np.concatenate([x[i: i + 1], predicted3[0, :1]]),
+                    # np.concatenate([y[i: i + 1], predicted3[0, 1:2]]),
+                    color="0.4",
+                    alpha=0.75,
+                )
+                r2, t2 = predicted_action3[0, :1], predicted_action3[0, 1:2]
+                x2, y2 = polar2euclid(r2, t2)
+                ax.plot(
+                    np.concatenate([x1, x2]),
+                    np.concatenate([y1, y2]),
+                    # np.concatenate([x[i: i + 1], predicted4[0, :1]]),
+                    # np.concatenate([y[i: i + 1], predicted4[0, 1:2]]),
+                    color="0.15",
+                    alpha=0.75,
+                )
+            else:
+                ax.plot(
+                    np.concatenate([x[i: i + 1], predicted_action0[0, :1]]),
+                    np.concatenate([y[i: i + 1], predicted_action0[0, 1:2]]),
+                    np.concatenate([z[i: i + 1], predicted_action0[0, 2:3]]),
+                    color="0.9",
+                    alpha=0.75,
+                )
+                ax.plot(
+                    np.concatenate([x[i: i + 1], predicted_action1[0, :1]]),
+                    np.concatenate([y[i: i + 1], predicted_action1[0, 1:2]]),
+                    np.concatenate([z[i: i + 1], predicted_action1[0, 2:3]]),
+                    color="0.65",
+                    alpha=0.75,
+                )
+                ax.plot(
+                    np.concatenate([x[i: i + 1], predicted_action2[0, :1]]),
+                    np.concatenate([y[i: i + 1], predicted_action2[0, 1:2]]),
+                    np.concatenate([z[i: i + 1], predicted_action2[0, 2:3]]),
+                    color="0.4",
+                    alpha=0.75,
+                )
+                ax.plot(
+                    np.concatenate([x[i: i + 1], predicted_action3[0, :1]]),
+                    np.concatenate([y[i: i + 1], predicted_action3[0, 1:2]]),
+                    np.concatenate([z[i: i + 1], predicted_action3[0, 2:3]]),
+                    color="0.15",
+                    alpha=0.75,
+                )
+
+        # Plot the dots at each time step depending on the action taken
+        xs, ys = polar2euclid(all_possible_abs_states[:, 0], all_possible_abs_states[:, 1])
+
+        if self.intern_dim == 2:
+            ax.scatter(
+                xs,
+                ys,
+                # all_possible_abs_states[:, 0],
+                # all_possible_abs_states[:, 1],
+                marker="x",
+                edgecolors="k",
+                alpha=0.5,
+                s=50,
+            )
+        else:
+            ax.scatter(
+                all_possible_abs_states[:, 0],
+                all_possible_abs_states[:, 1],
+                all_possible_abs_states[:, 2],
+                marker="x",
+                depthshade=True,
+                edgecolors="k",
+                alpha=0.5,
+                s=50,
+            )
+
+        if self.intern_dim == 2:
+            axes_lims = [ax.get_xlim(), ax.get_ylim()]
+        else:
+            axes_lims = [ax.get_xlim(), ax.get_ylim(), ax.get_zlim()]
+
+        # Plot the legend for transition estimates
+        box1b = TextArea(
+            " Estimated transitions (action 0, 1, 2 and 3): ", textprops=dict(color="k")
+        )
+        box2b = DrawingArea(90, 20, 0, 0)
+        el1b = Rectangle((5, 10), 15, 2, fc="0.9", alpha=0.75)
+        el2b = Rectangle((25, 10), 15, 2, fc="0.65", alpha=0.75)
+        el3b = Rectangle((45, 10), 15, 2, fc="0.4", alpha=0.75)
+        el4b = Rectangle((65, 10), 15, 2, fc="0.15", alpha=0.75)
+        box2b.add_artist(el1b)
+        box2b.add_artist(el2b)
+        box2b.add_artist(el3b)
+        box2b.add_artist(el4b)
+
+        boxb = HPacker(children=[box1b, box2b], align="center", pad=0, sep=5)
+
+        anchored_box = AnchoredOffsetbox(
+            loc=3,
+            child=boxb,
+            pad=0.0,
+            frameon=True,
+            bbox_to_anchor=(0.0, 0.98),
+            bbox_transform=ax.transAxes,
+            borderpad=0.0,
+        )
+        ax.add_artist(anchored_box)
+
+        # plt.show()
+        plt.savefig("fig_base" + str(learning_algo.update_counter) + ".pdf")
+        matplotlib.pyplot.close("all")  # avoids memory leaks
+
+    def end(self):
+        pass
+
+
+if __name__ == "__main__":
+    env = MyEnv(size_x=4,
+                size_y=4,
+                higher_dim_obs=False,
+                debug=True,
+                device="cuda")
+    print(env.observe())
+
+    # import math
+    # mod_pi = lambda v: (v + math.pi) % (2 * math.pi) - math.pi
+    # x = np.arange(-20, 20, step=0.1)
+    # y = np.array(list(map(mod_pi, x)))
+    # z = np.fmod(x, 2 * math.pi)
+    # plt.plot(x, y, label="correct")
+    # plt.plot(x, z, label="wrong")
+    # plt.legend()
+    # plt.savefig("mod_pi.png", dpi=300)
+
---
+++ experiments/loop_maze_full/run.py
@@ -0,0 +1,198 @@
+import os
+import yaml
+import json
+import wandb
+import jsons
+import pprint
+import logging
+# import numpy as np
+from joblib import hash, dump, load
+
+
+import core.controller as bc
+from core.agent import NeuralAgent
+from core.policy import EpsilonGreedyPolicy
+from core.learning_algorithm import CRAR
+from core.utils.seed_everything import *
+from loop_maze_full import MyEnv
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
+
+    # Load config
+    with open("config.yaml") as f:
+        args = yaml.safe_load(f)
+
+    pprint.PrettyPrinter(indent=2).pprint(args)
+
+    experiment_args = args["experiment_args"]
+    env_args = args["env_args"]
+    train_args = args["train_args"]
+    logger_args = args["logger_args"]
+
+    rng = (
+        np.random.RandomState(2022)
+        if train_args["deterministic"]
+        else np.random.RandomState()
+    )
+
+    # --- Instantiate logger ---
+    logger = wandb.init(
+        mode="online" if logger_args["online_mode"] else "offline",
+        project="deer",
+        entity="kvu207",
+        config=json.loads(jsons.dumps(args)),
+    )
+
+    # --- Instantiate environment ---
+    env = MyEnv(
+        size_x=env_args["size_x"],
+        size_y=env_args["size_y"],
+        device=train_args["device"],
+        debug=False,
+        higher_dim_obs=env_args["higher_dim_obs"],
+    )
+
+    # --- Instantiate learning_algo ---
+    learning_algo = CRAR(
+        environment=env,
+        rho=train_args["rms_decay"],
+        rms_epsilon=train_args["rms_epsilon"],
+        momentum=train_args["momentum"],
+        clip_norm=train_args["clip_norm"],
+        beta2=train_args["beta2"],
+        C=train_args["C"],
+        radius=train_args["radius"],
+        freeze_interval=train_args["freeze_interval"],
+        batch_size=train_args["batch_size"],
+        update_rule=train_args["update_rule"],
+        random_state=rng,
+        high_int_dim=False,
+        internal_dim=train_args["internal_dim"],
+        wandb_logger=logger,
+        device=train_args["device"],
+    )
+
+    # --- Instantiate agent ---
+    agent = NeuralAgent(
+        environment=env,
+        learning_algo=learning_algo,
+        replay_memory_size=train_args["replay_memory_size"],
+        replay_start_size=max(
+            env.get_input_dims()[i][0] for i in range(len(env.get_input_dims()))
+        ),
+        batch_size=train_args["batch_size"],
+        random_state=rng,
+        train_policy=EpsilonGreedyPolicy(learning_algo, env.get_num_action(), rng, 1.0),
+        test_policy=EpsilonGreedyPolicy(learning_algo, env.get_num_action(), rng, 1.0),
+    )
+
+    # --- Create unique filename for FindBestController ---
+    h = hash(args, hash_name="sha1")
+    filename = "test_" + h
+    print("The parameters hash is: {}".format(h))
+    print("The parameters are: {}".format(args))
+
+    # As for the discount factor and the learning rate, one can update periodically the parameter of the epsilon-greedy
+    # policy implemented by the agent. This controllers has a bit more capabilities, as it allows one to choose more
+    # precisely when to update epsilon: after every X action, episode or epoch. This parameter can also be reset every
+    # episode or epoch (or never, hence the resetEvery='none').
+    agent.attach(
+        bc.EpsilonController(
+            initial_e=train_args["epsilon_start"],
+            e_decays=train_args["epsilon_decay"],
+            e_min=train_args["epsilon_min"],
+            evaluate_on="action",
+            periodicity=1,
+            reset_every="none",
+        )
+    )
+
+    agent.run(n_epochs=10, epoch_length=500)
+    print("end gathering data")
+
+    # --- Bind controllers to the agent ---
+    # Before every training epoch (periodicity=1), we want to print a summary of the agent's epsilon, discount and
+    # learning rate as well as the training epoch number.
+    agent.attach(bc.VerboseController(evaluate_on="epoch", periodicity=1))
+
+    # Every epoch end, one has the possibility to modify the learning rate using a LearningRateController. Here we
+    # wish to update the learning rate after every training epoch (periodicity=1), according to the parameters given.
+    agent.attach(
+        bc.LearningRateController(
+            initial_learning_rate=train_args["learning_rate"],
+            learning_rate_decay=train_args["learning_rate_decay"],
+            periodicity=1,
+        )
+    )
+
+    # Same for the discount factor.
+    agent.attach(
+        bc.DiscountFactorController(
+            initial_discount_factor=train_args["discount"],
+            discount_factor_growth=train_args["discount_inc"],
+            discount_factor_max=train_args["discount_max"],
+            periodicity=1,
+        )
+    )
+
+    # During training epochs, we want to train the agent after every [parameters.update_frequency] action it takes.
+    # Plus, we also want to display after each training episode (!= than after every training) the average bellman
+    # residual and the average of the V values obtained during the last episode, hence the two last arguments.
+    agent.attach(
+        bc.TrainerController(
+            evaluate_on="action",
+            periodicity=train_args["update_frequency"],
+            show_episode_avg_v_value=True,
+            show_avg_bellman_residual=True,
+        )
+    )
+
+    # We wish to discover, among all versions of our neural network (i.e., after every training epoch), which one
+    # seems to generalize the better, thus which one has the highest validation score. Here, we do not care about the
+    # "true generalization score", or "test score".
+    # To achieve this goal, one can use the FindBestController along with an InterleavedTestEpochControllers. It is
+    # important that the validationID is the same than the id argument of the InterleavedTestEpochController.
+    # The FindBestController will dump on disk the validation scores for each and every network, as well as the
+    # structure of the neural network having the best validation score. These dumps can then used to plot the evolution
+    # of the validation and test scores (see below) or simply recover the resulting neural network for your
+    # application.
+    agent.attach(
+        bc.FindBestController(
+            validation_id=MyEnv.VALIDATION_MODE,
+            unique_fname=filename,
+        )
+    )
+
+    # All previous controllers control the agent during the epochs it goes through. However, we want to interleave a
+    # "validation epoch" between each training epoch. For each validation epoch, we want also to display the sum of all
+    # rewards obtained, hence the showScore=True. Finally, we want to call the summarizePerformance method of ALE_env
+    # every [parameters.period_btw_summary_perfs] *validation* epochs.
+    agent.attach(
+        bc.InterleavedTestEpochController(
+            id=MyEnv.VALIDATION_MODE,
+            epoch_length=experiment_args["steps_per_test"],
+            periodicity=1,
+            show_score=True,
+            summarize_every=1,
+        )
+    )
+
+    # --- Run the experiment ---
+    try:
+        os.mkdir("params")
+    except Exception:
+        pass
+
+    dump(args, "params/" + filename + ".jldump")
+
+    agent.gathering_data = False
+    agent.run(
+        n_epochs=experiment_args["epochs"],
+        epoch_length=experiment_args["steps_per_epoch"],
+    )
+
+    # --- Show results ---
+    basename = "scores/" + filename
+    scores = load(basename + "_scores.jldump")
+    print(scores)
